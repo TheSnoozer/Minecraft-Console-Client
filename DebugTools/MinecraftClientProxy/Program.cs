4b6bb0367d4811f06c8e2aa941c535711e6c02d1
TheSnoozer <s.lange@alumni.tu-berlin.de>
2019-09-28 11:06:45 +0200

    allow users to specify serverIp/serverPort for DebugTools via command-Input

--- DebugTools/MinecraftClientProxy/Program.cs
+++ DebugTools/MinecraftClientProxy/Program.cs
@@ -12,13 +12,20 @@ namespace MinecraftClientProxy
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter Minecraft Server-IP (defaults to localhost):");
+            var readLine = Console.ReadLine();
+            string serverIp = String.IsNullOrEmpty(readLine) ? "localhost" : readLine;
+
+            Console.WriteLine("Enter Minecraft Server-Port (defaults to 25565):");
+            int serverPort = int.TryParse(Console.ReadLine(), out serverPort) ? serverPort : 25565;
+
             Console.WriteLine("Waiting for client on port 25565...");
             TcpListener listener = new TcpListener(IPAddress.Any, 25565);
             listener.Start();
             TcpClient client = listener.AcceptTcpClient();
             
-            Console.WriteLine("Connecting to server on port 25566...");
-            TcpClient server = new TcpClient("localhost", 25566);
+            Console.WriteLine(String.Format("Connecting to server '{0}' on port {1}...", serverIp, serverPort));
+            TcpClient server = new TcpClient(serverIp, serverPort);
 
             Console.WriteLine("Starting proxy...\n");
             new PacketProxy(client, server).Run();
